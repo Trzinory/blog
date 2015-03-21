ed6ba88454b59dbfb2d88ff374cd71eef879d45a
Trziny <tarazedirony@gmail.com>
2015-03-21 23:39:31 +0800

    basic version upload

---
+++ js/blog_login.js
@@ -0,0 +1,7 @@
+    window.onload=function(){
+    	var coderequest=document.getElementById("code_request");
+    	var code=document.getElementById("code");
+    	coderequest.onclick=function(){
+      	code.src="";
+    	}
+  	}
